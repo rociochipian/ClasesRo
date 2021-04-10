ac66491ad6b2cebf9e542d3cad77f2d61d29de1c
Juan Brito <juan.a.brito90@gmail.com>
2021-04-10 18:04:26 -0300

    Primeros pasos en Android.

--- android/app/src/main/java/ar/edu/utn/frba/mobile/clases/ui/main/MainFragment.kt
+++ android/app/src/main/java/ar/edu/utn/frba/mobile/clases/ui/main/MainFragment.kt
@@ -1,15 +1,21 @@
 package ar.edu.utn.frba.mobile.clases.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ar.edu.utn.frba.mobile.clases.R
 import ar.edu.utn.frba.mobile.clases.databinding.MainFragmentBinding
 
+private const val MAIN_FRAGMENT_ARG_LABEL = "MAIN_FRAGMENT_ARG_LABEL"
+
 class MainFragment : Fragment() {
+    private var label: String? = null
+    private var listener: OnFragmentInteractionListener? = null
     private lateinit var viewModel: MainViewModel
 
     private var _binding: MainFragmentBinding? = null
@@ -18,7 +24,18 @@ class MainFragment : Fragment() {
     private val binding get() = _binding!!
 
     companion object {
-        fun newInstance() = MainFragment()
+        fun newInstance(title: String) = MainFragment().apply {
+            arguments = Bundle().apply {
+                putString(MAIN_FRAGMENT_ARG_LABEL, title)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            label = it.getString(MAIN_FRAGMENT_ARG_LABEL)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -31,5 +48,39 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.resources = MainViewModelResources(context!!)
+        // Forma clásica de encontrar una vista por id
+        view.findViewById<TextView>(R.id.textView).text = label
+        // Forma nueva con View Bindings
+        binding.button.setOnClickListener {
+            onButtonPressed()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+
+        super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    private fun onButtonPressed() {
+        listener?.onButtonTapped(viewModel.salute)
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onButtonTapped(salute: String)
     }
 }
